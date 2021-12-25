edf8d1f97ac674460c02576edd3e7ec3f5280dd5
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2021-12-25 23:01:19 +0800

    Create 2_6.c

---
+++ 2_6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  int toes;
+
+  toes = 10;
+
+  printf("toes is %d , twice toes is %d , toes squared is %d ./n",toes,toes * 2,toes * toes);
+
+  return 0 ;
+}
