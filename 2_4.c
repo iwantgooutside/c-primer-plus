ee154bc504ea0fe7099b049918bc14711345c9d8
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2021-12-25 22:14:08 +0800

    Create 2_4.c

---
+++ 2_4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void jolly(void);
+void deny(void);
+
+int main()
+{
+  jolly();
+  jolly();
+  jolly();
+  deny();
+
+  return 0 ;
+
+}
+
+void jolly(void)
+{
+  printf("For he's a jolly good fellow!\n");
+}
+
+void deny(void)
+{
+  printf("Which nobody can deny!\n");
+}
