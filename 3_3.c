2d8e4fb92cffcc2f9b74a321e1fdc94f48741d7a
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2021-12-28 22:49:12 +0800

    Create 3_3.c

---
+++ 3_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("%c",7);
+  /* or you could use this : printf("\a")*/
+  printf("Startled by the sudden sound,Sally shouted,\n");
+  printf("\"By the Great Pumpkin,what was that!\"\n");
+  return 0;
+}
