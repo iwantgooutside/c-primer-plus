3bb8ad1568719b2d71607fd3bbb585f020f51336
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2021-12-28 22:36:59 +0800

    Create 3_2.c

---
+++ 3_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+  int num;
+  printf("please enter your number :____");
+  printf("\b\b\b\b");
+  scanf("%d",&num);
+  printf("your number is char : %c \n",num);
+  return 0 ;
+}
