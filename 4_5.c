ee07a1bf4cd5499a594d07fe8531f01e62706ca0
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2021-12-30 22:18:23 +0800

    Update 4_5.c

--- 4_5.c
+++ 4_5.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(void)
+{
+  float f_speed,f_size;
+  printf("please enter speed:____\b\b\b\b");
+  scanf("%f",&f_speed);
+  printf("please enter size:____\b\b\b\b");
+  scanf("%f",&f_size);
+  printf("At %.2f megabits per second,a file of %.2f megabytes\n"
+        "downloads in %.2f seconds\n",f_speed,f_size,f_size * 8 / f_speed);
+  return 0 ;
+}
