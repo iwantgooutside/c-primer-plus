4263098b1d05b7b432bcff628c08d78fe59f61fd
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2022-01-02 19:20:02 +0800

    Update 5_3.c

--- 5_3.c
+++ 5_3.c
@@ -4,6 +4,12 @@ int main(void)
   int i_day;
   printf("please enter your days:____\b\b\b\b");
   scanf("%d",&i_day);
-  printf("%d days are %d weeks and %d days.\n",i_day,i_day/7,i_day%7);
+  while (i_day >0)
+  {
+    printf("%d days are %d weeks and %d days.\n",i_day,i_day/7,i_day%7);
+    printf("please enter your days:____\b\b\b\b");
+    scanf("%d",&i_day);
+  }
   return 0 ;
 }
+
