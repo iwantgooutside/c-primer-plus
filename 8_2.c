a3dba9e057b2471ffa9330287b84722cf1725478
legosuperman <88311520+iwantgooutside@users.noreply.github.com>
2022-01-24 18:37:48 +0800

    Create 8_2.c

---
+++ 8_2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+  int i_ch;
+  int i_index = 0 ;
+  while ((i_ch = getchar()) != EOF)
+  {
+    if (i_index < 10)
+    {
+      i_index++;
+      switch (i_ch)
+      {
+        case ' ' :
+          continue;
+        case '\n' :
+          printf("\n");
+          printf("\\n ");
+          printf("%-4d",(int)'\n');
+          break;
+        case '\t' :
+          printf("\\t ");
+          printf("%-4d",(int)'\t');
+          break;
+        default:
+          printf("%c ",i_ch);
+          printf("%-4d",i_ch);
+          break;
+      }
+    }
+    else
+    {
+      i_index = 0;
+      printf("\n");
+    }
+  }
+  return 0 ;
+
+}
